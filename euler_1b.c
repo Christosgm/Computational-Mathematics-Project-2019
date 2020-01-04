ae93c8aff6aab7f2faa3ffc2a6ce962c5c7a18f1
Christos Georgiou Mousses <x.georgiou.mousses@gmail.com>
2020-01-04 03:50:43 +0200

    Comments Added

--- euler_1b.c
+++ euler_1b.c
@@ -3,13 +3,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+//Compute the derivative.
 double call_fun(int position, long double  x1, long double  x2, long double  y1, long double  y2, long double  c1, long double  c2, long double  fx, long double fy, long double  nz)
 {
 
-	/*long double  fx = 0.0;
-	long double  fy = 0.0; 
-	long double  nz = 3496;*/ 
-
+	//Constants
 	long double  Dx = 11835.0;
 	long double  Dy = 8339.0;
 	long double  Dc = 15331.0;
@@ -17,6 +16,7 @@ double call_fun(int position, long double  x1, long double  x2, long double  y1,
 	long double  ma = 113000.0; 
 	long double  mz = 357000000.0;
 
+	//Functions for each of the vector's [x1', x2', y1', y2', c1', c2'] position.
 	if(position == 0)
 	{
 		return x2;
@@ -45,50 +45,79 @@ double call_fun(int position, long double  x1, long double  x2, long double  y1,
 		return s;
 	}
 
+	//If anything goes bad.
 	return -1.0;
 }
 
 
 int main(int argc, char * argv[])
 {
+
+	//The vector [fx, fy, nz] is passed as argument values according to the user.
+	//In the assignment only [3496, 0, 0], [0, -3496, 0] and [0, 0, -3496] are needed.
+
+	if(argc < 4)
+	{
+		printf("Not enough arguments. Exiting...");
+		exit(-1);
+	} 
+
+	//Starting values of [x1, x2, y1, y2, c1, c2].
 	long double  solutions[6] = {3.496, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+
 	long double  functions[6] = {0.0};
+
+	//The step of Euler's Method.
 	long double  h = 0.1;
 
+	//Choose between Euler's and Improved Euler's Method.
 	int choice;
-	printf("Do you want Euler\'s Method(1) or Improved Euler\'s Method(2)?");
-	scanf("%d", &choice);
-
-
-	int k;
-	for(k = 0; k<=6000; k++)
+	printf("Do you want Euler\'s Method(1) or Improved Euler\'s Method(2)?\nChoice: ");
+	do
 	{
+		scanf("%d", &choice);
+		printf("Please enter 1 or 2...\nChoice: ");
+	}
+	while(choice == 1 && choice == 2);
+
+	int k; //Number of loops.
+	for(k = 0; k<=6000; k++)//For 6000 loops (We want time equal to 600 and the step is 0.1)
+	{
+		//Depending the position of the vector, we want to compute the according derivative.
 		int position;
+
+		//Compute the derivative for each of the vector's position. [x1', x2', y1', y2', c1', c2'] 
 		for(position = 0; position < 6; position++)
 		{
 			functions[position] = call_fun(position, solutions[0], solutions[1], solutions[2], solutions[3], solutions[4], solutions[5], atof(argv[1]), atof(argv[2]), atof(argv[3]));
+			
+			//if Implroved Euler's Method was chosen, then the computation of [x1 + (h/2)*x1',x2 + (h/2)*x2', y1 + (h/2)*y1', y1 + (h/2)*y2', c1 + (h/2)*c1', c1 + (h/2)*c2'] is also needed.
 			if(choice == 2)
 			{
 				functions[position] = call_fun(position, solutions[0] + (h/2)*functions[position], solutions[1]+ (h/2)*functions[position], solutions[2]+ (h/2)*functions[position], solutions[3] + (h/2)*functions[position], solutions[4] + (h/2)*functions[position], solutions[5]+ (h/2)*functions[position], atof(argv[1]), atof(argv[2]), atof(argv[3]));
 			}
-		}	
+		}
 
+		//Show the starting values.
 		if(k == 0)
 		{
 			printf("t = %Lf: [x1=%Lf x2=%Lf y1=%Lf y2=%Lf c1=%Lf c2=%Lf]\n", k*h, solutions[0], solutions[1], solutions[2], solutions[3], solutions[4], solutions[5]);
 		}
 		else
 		{
+
+			//Compute the vector [x1(t+1), x2(t+1), y1(t+1), y2(t+1), c1(t+1), c2(t+1)] = [x1(t), x2(t), y1(t), y2(t), c1(t), c2(t)] + h * [x1(t) + (h/2)*x1'(t), x2(t) + (h/2)*x2'(t), y1(t) + (h/2)*y1'(t), y2(t) + (h/2)*y2'(t), c1(t) + (h/2)*c1'(t), c2(t) + (h/2)*c2'(t)]
 			for(position = 0; position < 6; position++)
 			{
 				solutions[position] = solutions[position] + h * functions[position];
 			}
+
+			//Show each vector.
 			printf("t = %.1Lf: [x1=%.15Lf x2=%.15Lf y1=%.15Lf y2=%.15Lf c1=%.15Lf c2=%.15Lf]\n", k*h, solutions[0], solutions[1], solutions[2], solutions[3], solutions[4], solutions[5]);
 		}
 
 	}
+
 	return 0;
 }
-
-
-
--- euler_1d.c
+++ euler_1d.c
@@ -1,11 +1,12 @@
 #include <math.h>
-#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+
+//Compute the derivative.
 double call_fun(int position, long double  x1, long double  x2, long double  y1, long double  y2, long double  c1, long double  c2)
 {
-
+	//Constants
 	long double Kpx = 77480.0;
 	long double Kdx = 5000000.0;
 	long double Kpy = 60000.0;
@@ -25,6 +26,7 @@ double call_fun(int position, long double  x1, long double  x2, long double  y1,
 	long double  ma = 113000.0; 
 	long double  mz = 357000000.0;
 
+	//Functions for each of the vector's [x1', x2', y1', y2', c1', c2'] position.
 	if(position == 0)
 	{
 		return x2;
@@ -53,75 +55,70 @@ double call_fun(int position, long double  x1, long double  x2, long double  y1,
 		return s;
 	}
 
+	//If anything goes bad.
 	return -1.0;
 }
 
-
 int main(int argc, char * argv[])
 {
+	//The vector [fx, fy, nz] is now described by a closed-loop automatic control algorithm so the functions are changed accordingly.
+
+	//Starting values of [x1, x2, y1, y2, c1, c2].
 	long double  solutions[6] = {0.0, 0.0, -3.496, 0.0, 0.0, 0.0};
+
 	long double  functions[6] = {0.0};
+
+	//The step of Euler's Method.
 	long double  h = 0.1;
 
+	//Choose between Euler's and Improved Euler's Method.
 	int choice;
-	printf("Do you want Euler\'s Method(1) or Improved Euler\'s Method(2)?");
-	scanf("%d", &choice);
-
-
-	int k;
-	for(k = 0; k<=6000; k++)
+	printf("Do you want Euler\'s Method(1) or Improved Euler\'s Method(2)?\nChoice: ");
+	do
 	{
+		scanf("%d", &choice);
+		printf("Please enter 1 or 2...\nChoice: ");
+	}
+	while(choice != 1 || choice !=2);
+
+	int k; //Number of loops.
+	for(k = 0; k<=6000; k++)//For 6000 loops (We want time equal to 600 and the step is 0.1)
+	{
+		//Depending the position of the vector, we want to compute the according derivative.
 		int position;
+
+		//Compute the derivative for each of the vector's position. [x1', x2', y1', y2', c1', c2'] 
 		for(position = 0; position < 6; position++)
 		{
 			functions[position] = call_fun(position, solutions[0], solutions[1], solutions[2], solutions[3], solutions[4], solutions[5]);
+			
+			//if Implroved Euler's Method was chosen, then the computation of [x1 + (h/2)*x1',x2 + (h/2)*x2', y1 + (h/2)*y1', y1 + (h/2)*y2', c1 + (h/2)*c1', c1 + (h/2)*c2'] is also needed.
 			if(choice == 2)
 			{
 				functions[position] = call_fun(position, solutions[0] + (h/2)*functions[position], solutions[1]+ (h/2)*functions[position], solutions[2]+ (h/2)*functions[position], solutions[3] + (h/2)*functions[position], solutions[4] + (h/2)*functions[position], solutions[5]+ (h/2)*functions[position]);
 			}
 		}	
 
+		
+		//Show the starting values.
 		if(k == 0)
 		{
-			printf("t = %Lf: [x1=%Lf x2=%Lf y1=%Lf y2=%Lf c1=%Lf c2=%Lf]\n", k*h, solutions[0], solutions[1], solutions[2], solutions[3], solutions[4], solutions[5]);
+			printf("t = %.1Lf: [x1=%Lf x2=%Lf y1=%Lf y2=%Lf c1=%Lf c2=%Lf]\n", k*h, solutions[0], solutions[1], solutions[2], solutions[3], solutions[4], solutions[5]);
 		}
 		else
 		{
+
+			//Compute the vector [x1(t+1), x2(t+1), y1(t+1), y2(t+1), c1(t+1), c2(t+1)] = [x1(t), x2(t), y1(t), y2(t), c1(t), c2(t)] + h * [x1(t) + (h/2)*x1'(t), x2(t) + (h/2)*x2'(t), y1(t) + (h/2)*y1'(t), y2(t) + (h/2)*y2'(t), c1(t) + (h/2)*c1'(t), c2(t) + (h/2)*c2'(t)]
 			for(position = 0; position < 6; position++)
 			{
 				solutions[position] = solutions[position] + h * functions[position];
 			}
+			
+			//Show each vector.
 			printf("t = %.1Lf: [x1=%.15Lf x2=%.15Lf y1=%.15Lf y2=%.15Lf c1=%.15Lf c2=%.15Lf]\n", k*h, solutions[0], solutions[1], solutions[2], solutions[3], solutions[4], solutions[5]);
 		}
 
 	}
 
-
-
-
-	char * commandsForGnuplot[] = {"set title \"TITLEEEEE\"", "plot 'data.temp'"};
-    double xvals[5] = {1.0, 2.0, 3.0, 4.0, 5.0};
-    double yvals[5] = {5.0 ,3.0, 1.0, 3.0, 5.0};
-    FILE * temp = fopen("data.temp", "w");
-    /*Opens an interface that one can use to send commands as if they were typing into the
-     *     gnuplot command line.  "The -persistent" keeps the plot open even after your
-     *     C program terminates.
-     */
-    FILE * gnuplotPipe = popen ("gnuplot -persistent", "w");
-    int i;
-    for (i=0; i < 5; i++)
-    {
-    fprintf(temp, "%lf %lf \n", xvals[i], yvals[i]); //Write the data to a temporary file
-    }
-
-    for (i=0; i < 5; i++)
-    {
-    fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]); //Send commands to gnuplot one by one.
-    }
-
-
 	return 0;
 }
-
-
-
